7a39116ac3c852a43a80ad468f8dd04e5a5aba3f
ay30n591 <syhyeon591@gmail.com>
2023-11-03 14:58:51 +0900

    Initial commit

---
+++ src/main/java/com/jjans/BB/BbApplication.java
@@ -0,0 +1,13 @@
+package com.jjans.BB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjans/BB/BbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjans.BB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
