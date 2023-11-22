8073c81d881a36dcd468cb5baffd2d0b7df221d2
seongcheollee <jhk6520736@gmail.com>
2023-11-22 16:44:10 +0900

    test

--- src/main/java/com/jjans/BB/Entity/Feed.java
+++ src/main/java/com/jjans/BB/Entity/Feed.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Entity
 @Data
-public class Feed {
+public class Feed extends BaseTime{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
@@ -18,9 +18,9 @@ public class Feed {
     private Users user;
 
     private String content;
+
     private int feedLike;
     private int feedImage;
-    private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "feed_id", cascade = CascadeType.ALL) // 일대다 관계 설정 및 CascadeType.ALL로 댓글에 대한 변경을 피드에 전파
     private List<Comment> commentsList;
