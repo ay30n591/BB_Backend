efc2b5aea3f8c33b6b64badf248256f17464850f
ay30n591 <syhyeon591@gmail.com>
2023-12-11 17:05:34 +0900

    commit

--- src/main/java/com/jjans/BB/Config/SecurityConfig.java
+++ src/main/java/com/jjans/BB/Config/SecurityConfig.java
@@ -19,13 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
-@CrossOrigin(origins = "http://localhost:3000") // CORS 설정
 
 public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -37,10 +35,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         //httpBasic, csrf, formLogin, rememberMe, logout, session disable
-        http
-                .cors()
-                .and()
+        http.cors().and()
                 .httpBasic().disable()
                 .csrf().disable()
                 .formLogin().disable()
@@ -57,29 +54,30 @@ public class SecurityConfig {
                 .antMatchers("/api/v1/users/userTest").hasRole("USER")
                 .antMatchers("/api/v1/users/adminTest").hasRole("ADMIN")
                 .anyRequest().authenticated();
-                        //oauth2Login
+
+        //oauth2Login
         http.oauth2Login()
                 .authorizationEndpoint()
-                    .baseUri("/oauth2/authorize")
-                    .authorizationRequestRepository(cookieAuthorizationRequestRepository) // 인증 요청을 쿠키에 저장하고 검색
+                .baseUri("/oauth2/authorization")  // 소셜 로그인 url
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository)  // 인증 요청을 cookie 에 저장
 
                 .and()
                 .redirectionEndpoint()
-                    .baseUri("/oauth2/callback/*")  // 소셜 인증 후 redirect url
+                .baseUri("/oauth2/callback/*")  // 소셜 인증 후 redirect url
                 .and()
                 //userService()는 OAuth2 인증 과정에서 Authentication 생성에 필요한 OAuth2User 를 반환하는 클래스를 지정한다.
                 .userInfoEndpoint()
-                    .userService(customOAuth2UserService)
-
+                .userService(customOAuth2UserService)
                 // 회원 정보 처리
+
                 .and()
                 .redirectionEndpoint()
                 .baseUri("/*/oauth2/code/*")
 
 
                 .and()
-                .successHandler(oAuth2AuthenticationSuccessHandler) // 401
-                .failureHandler(oAuth2AuthenticationFailureHandler); // 403
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
 
         http.logout()
                 .clearAuthentication(true)
