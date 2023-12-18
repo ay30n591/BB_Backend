0cdc9a7de3718d025ab5dec551f8fe1a2226d65e
seongcheollee <jhk6520736@gmail.com>
2023-12-18 18:51:35 +0900

    cors 추가

--- src/main/java/com/jjans/BB/Config/SecurityConfig.java
+++ src/main/java/com/jjans/BB/Config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -39,7 +40,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         //httpBasic, csrf, formLogin, rememberMe, logout, session disable
-        http.cors().and()
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
                 .httpBasic().disable()
                 .csrf().disable()
                 .formLogin().disable()
