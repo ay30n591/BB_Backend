02cdf46e06a0c2bb2a2ba93ae0c9e58d00169846
seongcheollee <jhk6520736@gmail.com>
2023-12-19 16:04:45 +0900

    Failed to connect to service endpoint 해결

--- src/main/java/com/jjans/BB/BbApplication.java
+++ src/main/java/com/jjans/BB/BbApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = {
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+})
 @EnableJpaAuditing
 @CrossOrigin(origins = "http://localhost:3000") // CORS 설정
 
