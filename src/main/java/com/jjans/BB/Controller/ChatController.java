4ea767dea7b837d60796e2fb91bac6f36484b602
seongcheollee <jhk6520736@gmail.com>
2023-12-21 23:53:39 +0900

    jwt 인증 추가

--- src/main/java/com/jjans/BB/Controller/ChatController.java
+++ src/main/java/com/jjans/BB/Controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.jjans.BB.Controller;
 
 import com.jjans.BB.Config.Kafka.KafkaProducer;
+import com.jjans.BB.Config.Security.JwtTokenProvider;
 import com.jjans.BB.Dto.ChatDto;
 import com.jjans.BB.Dto.ChatRoomDto;
 import com.jjans.BB.Service.ChatRoomService;
@@ -8,12 +9,10 @@ import com.jjans.BB.Service.ChatService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -24,34 +23,28 @@ public class ChatController {
 
     private ChatRoomService chatRoomService;
     private ChatService chatService;
+    private final JwtTokenProvider jwtTokenProvider;
+
     private final KafkaProducer producer;
     @Autowired
-    public ChatController(ChatRoomService chatRoomService, ChatService chatService, KafkaProducer producer) {
+    public ChatController(ChatRoomService chatRoomService, ChatService chatService, JwtTokenProvider jwtTokenProvider, KafkaProducer producer) {
         this.chatRoomService = chatRoomService;
         this.chatService = chatService;
+        this.jwtTokenProvider = jwtTokenProvider;
         this.producer = producer;
     }
-    @SecurityRequirement(name = "bearerAuth")
-    @PostMapping(value="/v1/message", consumes = "application/json",produces = "application/json")
-    public ResponseEntity<?> sendMessage(@Valid @RequestBody ChatDto chatDto) {
-//        if(!chatRoomService.existsRoom(chatDto.getRoomId())){
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-//                    .body("Room with ID " + chatDto.getRoomId() + " not found.");
-//        }
-        ChatDto savedMessage = chatService.saveChatMessage(chatDto);
-        producer.sendMessage(chatDto);
-        return ResponseEntity.ok(chatDto);
-    }
-    @SecurityRequirement(name = "bearerAuth")
+
     @MessageMapping("/message")
-    public void sendSocketMessage(ChatDto chatDto) {
+    public void sendSocketMessage(ChatDto chatDto, @Header("Authorization") String Authorization) {
         if(!chatRoomService.existsRoom(chatDto.getRoomId())){
             return;
         }
-        //ChatDto savedMessage = chatService.saveChatMessage(chatDto);
-        producer.sendMessage(chatDto);
+        String email = jwtTokenProvider.extractUserEmail(Authorization);
+        ChatDto savedMessage = chatService.saveChatMessage(chatDto,email);
+        producer.sendMessage(savedMessage);
 
     }
+
     @SecurityRequirement(name = "bearerAuth")
     @GetMapping("/rooms")
     public List<ChatRoomDto> getAllChatRoomsByUser() {
