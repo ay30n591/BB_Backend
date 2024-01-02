acdcedea43364bb0ee37d3b553ba77cf6ffee101
seongcheollee <jhk6520736@gmail.com>
2024-01-02 17:27:29 +0900

    readCount 추가

--- src/main/java/com/jjans/BB/Dto/ChatDto.java
+++ src/main/java/com/jjans/BB/Dto/ChatDto.java
@@ -8,6 +8,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,12 +19,16 @@ public class ChatDto {
     private String message;
     private String sender;
     private Long roomId;
+    private LocalDateTime createdTime;
+    private int readCount; // 기본값 2로 설정
 
 
     public ChatDto(Chat chat) {
         this.message = chat.getMessage();
         this.sender = chat.getSender().getNickName();
         this.roomId = chat.getChatRoom().getId();
+        this.readCount = chat.getReadCount();
+        this.createdTime = chat.getCreateDate();
     }
 
     public Chat toEntity() {
