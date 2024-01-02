f19c7b0d796e693a36483cc08c4be575766d5a35
seongcheollee <jhk6520736@gmail.com>
2024-01-02 17:28:36 +0900

    decreaseReadCount 기능 추가

--- src/main/java/com/jjans/BB/Service/ChatService.java
+++ src/main/java/com/jjans/BB/Service/ChatService.java
@@ -1,10 +1,15 @@
 package com.jjans.BB.Service;
 
 import com.jjans.BB.Dto.ChatDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ChatService {
-    ChatDto saveChatMessage(ChatDto chat);
-//    List<ChatDto> getChatMessagesByRoomId(Long roomId, int page, int size);
+    ChatDto saveChatMessage(ChatDto chat, String email);
+    List<ChatDto> getChatMessagesByRoomIdWithPaging(Long roomId, int page, int size);
+    void decreaseReadCount(Long roomId, String email);
+
+
 }
