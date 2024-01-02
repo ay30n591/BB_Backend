310d408feafca74c172b02e6261535fffed84421
seongcheollee <jhk6520736@gmail.com>
2024-01-02 17:29:16 +0900

    chatRoomConnectTemplate 추가

--- src/main/java/com/jjans/BB/Config/RedisRepositoryConfig.java
+++ src/main/java/com/jjans/BB/Config/RedisRepositoryConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
@@ -31,4 +32,17 @@ public class RedisRepositoryConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+
+
+    @Bean
+    public RedisTemplate<String, String> chatRoomConnectTemplate() {
+        RedisTemplate<String, String> chatRoomTemplate = new RedisTemplate<>();
+        chatRoomTemplate.setConnectionFactory(redisConnectionFactory());
+        chatRoomTemplate.setKeySerializer(new StringRedisSerializer());
+        chatRoomTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return chatRoomTemplate;
+    }
+
+    // chatroomtemplate 생성
 }
