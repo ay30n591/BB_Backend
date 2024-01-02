ffba6fe7b100652d8e29dff323bb3287f62b59ff
seongcheollee <jhk6520736@gmail.com>
2024-01-02 17:28:05 +0900

    페이징 기능 이용하여 메세지 불러오는 쿼리문 작성

--- src/main/java/com/jjans/BB/Repository/ChatRepository.java
+++ src/main/java/com/jjans/BB/Repository/ChatRepository.java
@@ -1,10 +1,13 @@
 package com.jjans.BB.Repository;
 
+import com.jjans.BB.Dto.ChatDto;
 import com.jjans.BB.Entity.Chat;
 import com.jjans.BB.Entity.ChatRoom;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,11 @@ import java.util.List;
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 //    Page<Chat> findByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom, Pageable pageable);
+    @Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :roomId")
+    Page<Chat> getChatMessagesByRoomId(@Param("roomId") Long roomId, Pageable pageable);
+
+    @Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :roomId")
+    List<Chat> getChatByRoomId(@Param("roomId") Long roomId);
+
+
 }
