37b67742c6b88d0620a9ba327d7ea4eadb29da43
seongcheollee <jhk6520736@gmail.com>
2024-01-02 17:29:30 +0900

    Command에 따른 동작 추가

--- src/main/java/com/jjans/BB/Config/Kafka/StompHandler.java
+++ src/main/java/com/jjans/BB/Config/Kafka/StompHandler.java
@@ -1,21 +1,19 @@
 package com.jjans.BB.Config.Kafka;
 
 import com.jjans.BB.Config.Security.JwtTokenProvider;
+import com.jjans.BB.Service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -24,34 +22,56 @@ import java.util.List;
 public class StompHandler implements ChannelInterceptor {
 
     private final JwtTokenProvider tokenProvider;
+    private final RedisTemplate<String, String> chatRoomConnectTemplate;
+    private final ChatService chatService;
     private final Logger logger = LoggerFactory.getLogger(StompHandler.class);
-    private static final ThreadLocal<String> currentUserEmailThreadLocal = new InheritableThreadLocal<>();
-
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String jwtToken = accessor.getFirstNativeHeader("Authorization");
 
-        if (StompCommand.CONNECT == accessor.getCommand()){
-            String jwtToken = accessor.getFirstNativeHeader("Authorization");
-            logger.info("webSocket token is {}", jwtToken);
+        switch (accessor.getCommand()){
+            case CONNECT:
+                // jwtToken이 null 또는 빈 문자열이 아닌 경우에만 validateToken 호출
+                if (jwtToken != null && !jwtToken.isEmpty()) {
+                    tokenProvider.validateToken(jwtToken);
 
-            // jwtToken이 null 또는 빈 문자열이 아닌 경우에만 validateToken 호출
-            if (jwtToken != null && !jwtToken.isEmpty()) {
+                } else {
+                    logger.warn("JWT Token is null or empty");
+                }
+                break;
+            case SUBSCRIBE:
+                // userNickname 불러오기
+                String destination = accessor.getDestination();
+                int lastIndex = destination.lastIndexOf('/');
+                String roomId = destination.substring(lastIndex + 1);
 
-                tokenProvider.validateToken(jwtToken);
-                String userEmail = tokenProvider.extractUserEmail(jwtToken);
-                logger.info("User email extracted from token: {}", userEmail);
-                currentUserEmailThreadLocal.set(userEmail);
+                String email = tokenProvider.extractUserEmail(jwtToken);
+                logger.info("roomId is {}", roomId);
+                String key = "chatroom:" + roomId;
 
-            } else {
-                logger.warn("JWT Token is null or empty");
-            }
+
+                chatRoomConnectTemplate.opsForValue().set(key,tokenProvider.extractUserEmail(jwtToken));
+                logger.info("webSocket lastIndex is {}", roomId);
+
+               //  읽지 않은 채팅 읽음 처리
+                chatService.decreaseReadCount(Long.valueOf(roomId), email);
+//
+//                Set<String> keys = chatRoomConnectTemplate.keys("chatroom:*");
+//                for (String k : keys) {
+//                    String value = chatRoomConnectTemplate.opsForValue().get(k);
+//                    System.out.println("Key: " + k + ", Value: " + value);
+//                }
+                int size  = chatRoomConnectTemplate.keys(key).size();
+
+
         }
 
-         return message;
+
+
+        return message;
     }
 
-    public static String getCurrentUserEmail() {
-        return currentUserEmailThreadLocal.get();
-    }
+
+
 }
