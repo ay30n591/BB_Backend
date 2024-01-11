fd8891465d569335c368bba41f9dbd8e4a871945
seongcheollee <jhk6520736@gmail.com>
2024-01-11 23:12:34 +0900

    chatType enum 클래스 추가

--- src/main/java/com/jjans/BB/Dto/ChatDto.java
+++ src/main/java/com/jjans/BB/Dto/ChatDto.java
@@ -1,5 +1,6 @@
 package com.jjans.BB.Dto;
 
+import com.amazonaws.services.kms.model.MessageType;
 import com.jjans.BB.Entity.Chat;
 import com.jjans.BB.Entity.ChatRoom;
 import com.jjans.BB.Entity.Users;
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ChatDto {
-
+    private Chat.ChatType chatType; // 메시지 타입 추가
     private String message;
     private String sender;
     private Long roomId;
@@ -24,6 +25,7 @@ public class ChatDto {
 
 
     public ChatDto(Chat chat) {
+        this.chatType = chat.getChatType();
         this.message = chat.getMessage();
         this.sender = chat.getSender().getNickName();
         this.roomId = chat.getChatRoom().getId();
@@ -33,6 +35,7 @@ public class ChatDto {
 
     public Chat toEntity() {
         Chat chat = Chat.builder()
+                .chatType(chatType)
                 .message(message)
                 .build();
         return chat;
