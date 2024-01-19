e58f2169cae4eb5b58723cecd01d31eb85feebe8
seongcheollee <jhk6520736@gmail.com>
2024-01-19 21:32:51 +0900

    searchInfo 제거

--- src/main/java/com/jjans/BB/Dto/UserResponseDto.java
+++ src/main/java/com/jjans/BB/Dto/UserResponseDto.java
@@ -1,7 +1,6 @@
 package com.jjans.BB.Dto;
 
 import com.jjans.BB.Entity.Users;
-import com.jjans.BB.Entity.UsersDocument;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,34 +31,7 @@ public class UserResponseDto {
         }
     }
 
-    @AllArgsConstructor
-    @Getter
-    @Builder
-    public static class searchInfo {
-//        private long id;
-        private String email;
-        private  String userName;
-        private  String nickName;
-        private String imgSrc;
-        public searchInfo searchUserInfo(UsersDocument usersDocument ) {
-//            this.id = usersDocument.getId();
-            this.email = usersDocument.getEmail();
-            this.userName = usersDocument.getUserName();
-            this.nickName = usersDocument.getNickName();
-            this.imgSrc = usersDocument.getImgSrc();
-            return this;
-        }
-    }
-    @Builder
-    public UserResponseDto(Long userId, String userName, String email, String nickName ) {
-    }
 
-    public static UserResponseDto from(UsersDocument usersDocument) {
-        return UserResponseDto.builder()
-                .userId(usersDocument.getId())
-                .userName(usersDocument.getUserName())
-                .nickName(usersDocument.getNickName())
-                .email(usersDocument.getEmail())
-                .build();
-    }
+
+
 }
