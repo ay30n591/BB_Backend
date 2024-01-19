f8b726be5f290179c7bfcf74972a08715ba09157
seongcheollee <jhk6520736@gmail.com>
2024-01-19 21:31:57 +0900

    readcount 최신화 수정

--- src/main/java/com/jjans/BB/Config/Kafka/StompHandler.java
+++ src/main/java/com/jjans/BB/Config/Kafka/StompHandler.java
@@ -1,6 +1,8 @@
 package com.jjans.BB.Config.Kafka;
 
 import com.jjans.BB.Config.Security.JwtTokenProvider;
+import com.jjans.BB.Entity.Users;
+import com.jjans.BB.Repository.UsersRepository;
 import com.jjans.BB.Service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +15,8 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 import java.util.Set;
 
 @Component
@@ -27,7 +28,8 @@ public class StompHandler implements ChannelInterceptor {
     private final JwtTokenProvider tokenProvider;
     private final RedisTemplate<String, String> chatRoomConnectTemplate;
     private final ChatService chatService;
-    private final KafkaProducer producer;
+    private final UsersRepository usersRepository;
+
 
     private final Logger logger = LoggerFactory.getLogger(StompHandler.class);
     @Override
@@ -35,78 +37,58 @@ public class StompHandler implements ChannelInterceptor {
         final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         String jwtToken = accessor.getFirstNativeHeader("Authorization");
 
-        switch (accessor.getCommand()){
+        switch (accessor.getCommand()) {
             case CONNECT:
                 // jwtToken이 null 또는 빈 문자열이 아닌 경우에만 validateToken 호출
                 if (jwtToken != null && !jwtToken.isEmpty()) {
                     tokenProvider.validateToken(jwtToken);
-
                 } else {
                     logger.warn("JWT Token is null or empty");
                 }
                 break;
+
             case SUBSCRIBE:
 
                 String destination = accessor.getDestination();
                 int lastIndex = destination.lastIndexOf('/');
                 String roomId = destination.substring(lastIndex + 1);
-
                 String email = tokenProvider.extractUserEmail(jwtToken);
                 String key = "chatroom:" + roomId;
-                logger.info("webSocket lastIndex is {}", roomId);
 
+                logger.info("Room ID is {}", roomId);
 
                 chatRoomConnectTemplate.opsForSet().add(key, email);
                 Set<String> users = chatRoomConnectTemplate.opsForSet().members(key);
-                for (String user : users) {
-                    System.out.println("User: " + user);
-                }
-                int size  = users.size();
-                logger.info("SUBSCRIBE size is {}", size);
+                logger.info("SUBSCRIBE size is {}", users.size());
 
-               //  읽지 않은 채팅 읽음 처리
-                chatService.decreaseReadCount(Long.valueOf(roomId), email);
+                Users user = usersRepository.findByEmail(email)
+                        .orElseThrow(() -> new UsernameNotFoundException("No authentication information."));
 
-                // 변경내용 상대방 화면에 -> 새로운 메세지를 보내. 해당 메세지를 받으면 메세지를 다시 불러와.
+                //  읽지 않은 채팅 읽음 처리
+                chatService.decreaseReadCount(Long.valueOf(roomId), email, users.size());
+
+                // 입장 메세지 전송
+                chatService.updateMessage(Long.valueOf(roomId), user.getNickName(), users.size());
                 break;
+
             case UNSUBSCRIBE:
-                destination = accessor.getDestination();
-                lastIndex = destination.lastIndexOf('/');
-                roomId = destination.substring(lastIndex + 1);
+
+                String subscriptionId = accessor.getSubscriptionId();
+                lastIndex = subscriptionId.lastIndexOf('/');
+                roomId = subscriptionId.substring(lastIndex + 1);
                 key = "chatroom:" + roomId;
                 email = tokenProvider.extractUserEmail(jwtToken);
+                logger.info("Room ID is {}", roomId);
+
+
+                // 채팅방 현재 인원 최신화.
                 chatRoomConnectTemplate.opsForSet().remove(key, email);
                 users = chatRoomConnectTemplate.opsForSet().members(key);
-                size  = users.size();
-                logger.info("UNSUBSCRIBE. size is {}", size);
+                logger.info("UNSUBSCRIBE. size is {}", users.size());
                 break;
-//                chatRoomConnectTemplate.opsForValue().getOperations().delete(key, email);
 
-//                if (size > 1){
-//                    List<ChatDto> chatMessages =  chatService.getChatMessagesByRoomIdWithPaging(Long.valueOf(roomId), 1, size);
-//                    logger.info("chatMessages is {}", chatMessages);
-//
-//                    for (ChatDto chatMessage : chatMessages) {
-//
-//                        producer.sendMessage(chatMessage);
-//                    }
-                }
-//                else {
-
-//                    List<ChatDto> chatMessages =  chatService.getChatMessagesByRoomIdWithPaging(Long.valueOf(roomId), 1, size);
-//                    logger.info("chatMessages is {}", chatMessages);
-//
-//                    for (ChatDto chatMessage : chatMessages) {
-//                        producer.sendMessage(chatMessage);
-//                    }
-                //}
-     //   }
-
-
+        }
 
         return message;
     }
-
-
-
 }
